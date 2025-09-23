032cd68f32fb123211691047cbac8e5ddfd838fe
LucasNGRE <lucas12negre@gmail.com>
2025-09-23 14:58:52 +0200

    style: adjust indentation for consistency in user registration route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
+  UsersController.postNew(req, res);
 });
 
 export default router;
