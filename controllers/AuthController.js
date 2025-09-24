e5e73136862792eac98c10f9365e251ad75a9b6a
LucasNGRE <lucas12negre@gmail.com>
2025-09-24 09:50:34 +0200

    refactor: improve formatting and consistency in AuthController methods

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,36 +1,37 @@
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 
 class AuthController {
   static async getConnect(req, res) {
-  const authHeader = req.headers.authorization || '';
-  if (!authHeader.startsWith('Basic ')) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    const authHeader = req.headers.authorization || '';
+    if (!authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64Creds = authHeader.split(' ')[1];
+    const [email, password] = Buffer.from(base64Creds, 'base64')
+      .toString()
+      .split(':');
+
+    if (!email || !password) return res.status(401).json({ error: 'Unauthorized' });
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const user = await dbClient.db.collection('users').findOne({
+      email,
+      password: hashedPassword,
+    });
+
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+
+    return res.status(200).json({ token });
   }
-
-  const base64Creds = authHeader.split(' ')[1];
-  const [email, password] = Buffer.from(base64Creds, 'base64')
-    .toString()
-    .split(':');
-
-  if (!email || !password) return res.status(401).json({ error: 'Unauthorized' });
-
-  const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-  const user = await dbClient.db.collection('users').findOne({
-    email,
-    password: hashedPassword,
-  });
-
-  if (!user) return res.status(401).json({ error: 'Unauthorized' });
-
-  const token = uuidv4();
-  await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-
-  return res.status(200).json({ token });
-}
-
 
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
@@ -58,7 +59,7 @@ class AuthController {
 
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
 
-    return res.status(200).json({ id: user._id, email: user.email });
+    return res.status(200).json({ id: user._id.toString(), email: user.email });
   }
 }
 
